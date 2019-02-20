12e8c4c7922f91a9ed63eb0341e1d5ff6523bb54
vijayanemala <vijaya@olivetech.net>
2019-02-20 18:43:48 +0530

    Reducers and actions

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,10 @@
+const intialState = {
+    test: 'Test'
+}
+
+export default function (state = intialState, action ) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
